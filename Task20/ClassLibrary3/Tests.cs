55b24bebd4451a781829b3032094f8b7afd7eb1c
Katya Astakhova <KatyaAstakhova@coherentsolutions.com>
2019-02-18 13:00:35 +0300

    Task70 - Page Object pattern

---
+++ Task20/ClassLibrary3/Tests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using OpenQA.Selenium.Chrome;
+
+namespace Task70_POM
+{
+    [TestFixture]
+    public class Tests
+    {
+        public IWebDriver Driver { get; set; }
+        public WebDriverWait Wait { get; set; }
+
+        [SetUp]
+        public void SetupTest()
+        {
+            
+            Driver = new ChromeDriver(@"D:\Automation");
+            Wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(30));
+        }
+
+        [Test]
+        public void LoginUserTutBy()
+        {
+            PageObject tutby = new PageObject(Driver);
+            tutby.Navigate();
+            string username = "seleniumtests@tut.by";
+            string password = "123456789zxcvbn";
+            tutby.Login(username, password);
+
+            var loginState = tutby.IsUserLogin();
+            Assert.IsTrue(loginState);
+        }
+
+        [Test]
+        public void LogoutUserTutBy()
+        {
+            PageObject tutby = new PageObject(Driver);
+            tutby.Navigate();
+            string username = "seleniumtests@tut.by";
+            string password = "123456789zxcvbn";
+            tutby.Login(username, password);
+            tutby.Logout();
+
+            var logoutState = tutby.IsUserLogout();
+            Assert.IsTrue(logoutState);
+        }
+
+
+    }
+}
